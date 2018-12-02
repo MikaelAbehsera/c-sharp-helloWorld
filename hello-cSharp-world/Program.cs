20188d93454f6735a731d1f257129011e0eb9971
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-02 18:22:05 -0500

    first commit

---
+++ hello-cSharp-world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellocSharpworld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
