7b2b6e014465e96f1dd4203338dbb4b2d96a4e04
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-02 22:52:46 -0500

    added todo and hack comments

--- hello-cSharp-world/Program.cs
+++ hello-cSharp-world/Program.cs
@@ -4,13 +4,14 @@ namespace VariableScope
 {
     class Program
     {
+        //TODO: this is a todo comment
         static void Main(string[] args)
         {
             GreetPersons(0);
             GreetPersons(25, "John", "Jane", "Tarzan");
             Console.ReadKey();
         }
-
+       //HACK: this is a hack comment
         static void GreetPersons(int someUnusedParameter, params string[] names)
         {
             foreach (string name in names)
