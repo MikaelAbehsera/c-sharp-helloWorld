e2e2c755d072fdae24293367db6343a56ad0c618
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-19 17:24:56 -0500

    added conditionals

---
+++ conditionals.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Conditionals
+{
+  public static void Main()
+  {
+    int guess = 500;
+
+    // Write conditional here
+    if (guess == 500)
+    {
+      Console.WriteLine("Success!");
+    }
+
+  }
+}
