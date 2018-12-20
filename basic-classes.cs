ce25b2f9c0236047678d9482c924fb8f912d9814
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-20 12:44:37 -0500

    added car class and instantized it 3 times

--- basic-classes.cs
+++ basic-classes.cs
@@ -1,10 +1,20 @@
 using System;
-public class MainClass{
-    public static void Main(){
-        
-        // Test code
-        Console.WriteLine(car1.numTires);
-        Console.WriteLine(car2.year);
-        Console.WriteLine(car3.runs);
-    }
+public class MainClass
+{
+  public static void Main()
+  {
+    car car1 = new car();
+    car car2 = new car();
+    car car3 = new car();
+    // Test code
+    Console.WriteLine(car1.numTires);
+    Console.WriteLine(car2.year);
+    Console.WriteLine(car3.runs);
+  }
 }
+class car
+{
+  public int numTires = 4;
+  public int year = 2000;
+  public bool runs = true;
+}    
