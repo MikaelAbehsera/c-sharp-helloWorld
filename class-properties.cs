643c26f3c0a7e50e897af6d2a1153fbebab692cb
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-20 13:06:43 -0500

    added car class to temp

---
+++ class-properties.cs
@@ -0,0 +1,25 @@
+using System;
+
+//Write Vehicle class here
+
+public class MainClass
+{
+  public static void Main()
+  {
+    //Create vehicles here
+
+
+
+
+
+    // Test code
+    Console.WriteLine(car.Type);
+    Console.WriteLine(oldcar.Runs);
+    Console.WriteLine(bike.NumTires);
+  }
+}
+
+class car
+{
+
+}
