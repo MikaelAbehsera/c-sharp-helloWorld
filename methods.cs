076cb85a002caeda7be155dc2c0a995bc24a0702
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-20 12:09:06 -0500

    added template to methods

---
+++ methods.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Methods
+{
+  public static void Main()
+  {
+
+    int x = 2;
+    int y = 2;
+    int a = foo(x, y);
+    Console.WriteLine(a);
+
+  }
+
+  //write method foo here
+
+  public static int Foo()
+  {
+
+  }
+
+
+}
