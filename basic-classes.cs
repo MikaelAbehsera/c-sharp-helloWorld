742bcdaf65cfb8623c82c338c091824fce8943e2
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-20 12:39:16 -0500

    template for classes example

---
+++ basic-classes.cs
@@ -0,0 +1,10 @@
+using System;
+public class MainClass{
+    public static void Main(){
+        
+        // Test code
+        Console.WriteLine(car1.numTires);
+        Console.WriteLine(car2.year);
+        Console.WriteLine(car3.runs);
+    }
+}
