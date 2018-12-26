c9d09188003992ca42981f9ef1e982e29b68e24d
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-26 09:26:28 -0500

    added shortest word practice example

---
+++ shortestWord/shortestWord.cs
@@ -0,0 +1,9 @@
+public class Kata
+{
+  public static int FindShort(string s)
+  {
+    // split words by spaces
+    // get word length
+    // store 2 variables ==> 1 shortest word index, 2 shortest word length for refrence
+  }
+}
