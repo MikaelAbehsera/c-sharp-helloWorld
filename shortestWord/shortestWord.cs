467a86c22200de46cbc003fca8fb5234f243b18a
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-26 10:00:54 -0500

    returning short length and not word

--- shortestWord/shortestWord.cs
+++ shortestWord/shortestWord.cs
@@ -6,17 +6,15 @@ public class Kata
     // get word length
     // store 2 variables ==> 1 shortest word index, 2 shortest word length for refrence
 
-    string spl = s.Split(" ");
-    int shortestWordLength = 0;
-    int shortestWordIndex = 0;
+    string[] spl = s.Split(" ");
+    int shortestWordLength = spl[0];
     
     for( int i = 0; i < spl.Length; i++ ) {
       if( spl[i].Length < shortestWordLength ) {
         shortestWordLength = spl[i].Length;
-        shortestWordIndex = i;
       }  
     }
 
-    return spl[shortestWordIndex];
+    return shortestWordLength;
   }
 }
