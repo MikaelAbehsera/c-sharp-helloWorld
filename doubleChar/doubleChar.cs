15a590180f70323cb5d4f85ce0aa853bb010854e
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-27 10:44:14 -0500

    added loop and output

--- doubleChar/doubleChar.cs
+++ doubleChar/doubleChar.cs
@@ -2,6 +2,14 @@ public class Kata
 {
   public static string DoubleChar(string s)
   {
-    // your code here
+    string[] str = s.Split("");
+    List<string> output = new List<string>();
+    foreach (string letter in str) {
+      output.Add(letter);
+      output.Add(letter);
+    }
+    output.Join("");
+
+    return output;
   }
 }
