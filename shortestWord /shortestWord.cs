09267a7533e01719fdc2570dcd35d57c99766f87
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-28 18:00:59 -0500

    fixed issue, loop starts with second int

--- shortestWord /shortestWord.cs
+++ shortestWord /shortestWord.cs
@@ -5,9 +5,9 @@ public class Kata
     // split string by words (aka spaces)
     string[] words = s.Split(" ");
     // make a variable to store the length
-    int shortest = 0;
+    int shortest = words[0].Length;
     // iterate over the words and store the shortest value
-    for( int i = 0; i < words.Length; i++ ) {
+    for( int i = 1; i < words.Length; i++ ) {
       if ( shortest > words[i].Length ) {
         shortest = words[i].Length;
       }
