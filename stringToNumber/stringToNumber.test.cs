319adde9039569b82eed69957e1e5d55bc06824e
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-28 16:41:08 -0500

    added tests, they pass

---
+++ stringToNumber/stringToNumber.test.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+  using System;
+  [TestFixture]
+  public class StringToNumberTest
+  { 
+    [Test]
+    public void test1()
+    {
+      Assert.AreEqual(1234 , Kata.StringToNumber("1234"));
+    }
+
+    [Test]
+    public void test2()
+    {
+      Assert.AreEqual(605 , Kata.StringToNumber("605"));
+    }
+
+    [Test]
+    public void test3()
+    {
+      Assert.AreEqual(1405 , Kata.StringToNumber("1405"));
+    }
+
+    [Test]
+    public void test4()
+    {
+      Assert.AreEqual(-7 , Kata.StringToNumber("-7"));
+    }  
+  }
