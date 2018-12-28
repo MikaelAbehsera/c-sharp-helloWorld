d1184e92791b26ac7397008a18d67f8927855e2c
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-28 15:29:46 -0500

    added class and func for vowel count + tests

---
+++ vowelCount/vowelCount.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+
+        // Your code here
+
+        return vowelCount;
+    }
+}
