48d3f29625b0e41724ea5a307ad38c08b64a3072
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-29 12:29:23 -0500

    added loop

--- dnaCompiler/dnaCompiler.cs
+++ dnaCompiler/dnaCompiler.cs
@@ -1,7 +1,22 @@
+using System;
 public class DnaStrand 
     {
         public static string MakeComplement(string dna)
         {
-            //Your code
+          List<string> dnaString = new List<string>();
+
+          foreach (char d in dna) {
+            if( d.ToString() == "A" ) {
+              dnaString.Add("T");
+            } else if ( d.ToString() == "T" ) {
+              dnaString.Add("A");
+            } else if ( d.ToString() == "G" ) {
+              dnaString.Add("C"); 
+            } else if ( d.ToString() == "C" ) {
+              dnaString.Add("G");
+            }
+          }
+
+          return dna;
         }
     }
