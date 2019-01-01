59b03964c4e010d979a158bd21019c983a393b12
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-01 13:20:13 -0500

    added loop logic to add new logs to dictionary

--- countingDuplicates/countingDuplicates.cs
+++ countingDuplicates/countingDuplicates.cs
@@ -4,6 +4,27 @@ public class Kata
 {
     public static int DuplicateCount(string str)
     {
-        return -1;
+        // make a dictionary where we can store results
+        Dictionary<string, int> book = new Dictionary<string, int>();
+
+        // split str into a list
+        string[] all = str.Split(""); 
+
+        // loop over all and store results in the dictionary 
+        for ( int i = 0; i < all.Length; i++ )
+        {
+            // check if we have a log of this letter
+            if ( dict.ContainsKey( all[i] ) )
+            {
+                //  add 1 to that log
+                book[all[i]]++;
+            } else
+            {
+                // if it can not be found in the dictonary then add it
+                book.Add(all[i], 1);
+            }
+        }
+
+      
     }
 }
