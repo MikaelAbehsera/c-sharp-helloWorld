df1291ed4e84d22bd7563151a19b26b1952eecc4
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-04 15:44:48 -0500

    fixed bugs, need to return string and not array

--- weirdStringCase/weirdStringCase.cs
+++ weirdStringCase/weirdStringCase.cs
@@ -8,18 +8,17 @@ public class Kata
         int counter = 0;
         List<string> str = new List<string>();
 
-
         foreach (char c in s)
         {
 
             if (counter % 2 == 0)
             {
-                string letter = Char.ToUpper(c).ToString();
+                string letter = (Char.ToUpper(c)).ToString();
                 str.Add(letter);
                 Console.WriteLine(letter);
             } else
             {
-                string letter = Char.ToLower(c).ToString();
+                string letter = (Char.ToLower(c)).ToString();
                 str.Add(letter);
                 Console.WriteLine(letter);
             }
