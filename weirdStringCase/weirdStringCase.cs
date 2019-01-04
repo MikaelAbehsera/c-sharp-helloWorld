5bce0057cb905a41a483cb5806f589dc06e23d44
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-04 15:00:23 -0500

    missing new keyword

--- weirdStringCase/weirdStringCase.cs
+++ weirdStringCase/weirdStringCase.cs
@@ -5,13 +5,19 @@ public class Kata
     public static string ToWeirdCase(string s)
     {
         int counter = 0;
+        List<string> str = new List<string>();
+
 
         foreach (char c in s)
         {
 
             if (counter % 2 == 0)
             {
-                c = Char.ToUpper(c);
+               char letter = Char.ToUpper(c);
+                str.add(letter);
+            } else
+            {
+                str.add(letter);
             }
 
             counter++;
