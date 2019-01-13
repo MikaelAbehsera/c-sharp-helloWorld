fd1b465f97955c7f1d3806e75f1022a450901a75
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-13 18:48:57 -0500

    added spliting number

--- descendingOrder/descendingOrder.cs
+++ descendingOrder/descendingOrder.cs
@@ -1,14 +1,15 @@
-string result = "12345";
-var intList = result.Select(digit => int.Parse(digit.ToString()));
-
 using System;
 
 public static class Kata
 {
 	public static int DescendingOrder(int num)
 	{
-		// Bust a move right here
-		return 0;
+		string result = "12345";
+		int[] intList = result.Select(digit => int.Parse(digit.ToString()));
+
+
+
+
 	}
 }
 
