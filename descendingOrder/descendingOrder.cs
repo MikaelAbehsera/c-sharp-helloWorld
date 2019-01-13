d722b1e5dce7576b96590cdb854979201813548a
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-13 18:26:04 -0500

    added new question

---
+++ descendingOrder/descendingOrder.cs
@@ -0,0 +1,14 @@
+string result = "12345";
+var intList = result.Select(digit => int.Parse(digit.ToString()));
+
+using System;
+
+public static class Kata
+{
+	public static int DescendingOrder(int num)
+	{
+		// Bust a move right here
+		return 0;
+	}
+}
+
