55fb4f7a08f734c9875ec84bcfb207f5d179b37e
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-13 18:17:31 -0500

    fixed index out of bounds

--- facebookLikes/facebookLikes.cs
+++ facebookLikes/facebookLikes.cs
@@ -9,10 +9,10 @@ public static class Kata
 	/// <returns></returns>
 	public static string Likes(string[] name)
 	{
-		if(name.Length == 1) return name[1] + " likes this";
+		if(name.Length == 1) return name[0] + " likes this";
 		else if(name.Length == 2) return name[0] + " and " + name[1] + " like this";
 		else if(name.Length == 3) return name[0] + ", " + name[1] + " and " + name[2] + " like this";
-		else if(name.Length > 3) return name[0] + ", " + name[1] + " and " + name.Length + " others like this";
+		else if(name.Length > 3) return name[0] + ", " + name[1] + " and " + (name.Length - 2) + " others like this";
 		else return "no one likes this";
 	}
 }
