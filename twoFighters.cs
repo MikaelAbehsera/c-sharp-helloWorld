d03353ffdce57e14b4535ea1b0ed9e3cbf431c43
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-12 23:32:40 -0500

    added new

---
+++ twoFighters.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+	public static string declareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker)
+	{
+		// Your code goes here. Have fun!
+	}
+}
