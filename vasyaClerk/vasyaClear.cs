8b5d3b3660a0fe1bc1bf89649a6c15ca30051c3e
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-14 11:25:47 -0500

    added dict loop and change counter

--- vasyaClerk/vasyaClear.cs
+++ vasyaClerk/vasyaClear.cs
@@ -6,19 +6,32 @@ public class Line
 	{
 		int bank = 0;
 		Dictionary<int, int> register = new Dictionary<int, int>();
-		register[100] = 0;
-		register[50] = 0;
-		register[25] = 0;
-		register[10] = 0;
-		register[5] = 0;
+		register.Add(100, 0);
+		register.Add(50, 0);
+		register.Add(25, 0);
+		register.Add(10, 0);
+		register.Add(5, 0);
 
-		// loop on dicionary and see if we have bills to return cash
-		foreach(KeyValuePair<int, int> entry in myDictionary) {
-	
+		foreach(int payment in peopleInLine) {
+			// loop on dicionary and see if we have bills to return cash
+			int change = payment;
+			foreach(KeyValuePair<int, int> bill in register) {
+				// do we need to return change?
+				if(payment - 25 > 0) {
+					if(bill.value > 0) {
+						// check if we can pay with this bill 
+						int times = (change % bill.key);
+						if(times > 0) {
+							change - (bill.value * times);
+						}
+					}
+				}
+			}
+			if(change > 0) {
+				return "NO";
+			}
 		}
-
-
-		}
-}
+		return "YES";
+	}
 }
 
