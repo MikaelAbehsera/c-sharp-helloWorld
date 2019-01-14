0e13fca912c90cf7893957fa17f07f145b927dee
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-14 10:51:24 -0500

    changing setup to work with dict and bills

--- vasyaClerk/vasyaClear.cs
+++ vasyaClerk/vasyaClear.cs
@@ -5,19 +5,20 @@ public class Line
 	public static string Tickets(int[] peopleInLine)
 	{
 		int bank = 0;
-		foreach(int payment in peopleInLine) {
-			
-			int change = payment;
-			change -= 25;
-			bank += 25;
-			if (bank - payment < 0) {
-				return "NO";
-			}
+		Dictionary<int, int> register = new Dictionary<int, int>();
+		register[100] = 0;
+		register[50] = 0;
+		register[25] = 0;
+		register[10] = 0;
+		register[5] = 0;
+
+		// loop on dicionary and see if we have bills to return cash
+		foreach(KeyValuePair<int, int> entry in myDictionary) {
+	
 		}
-		return "YES";
-	}
+
+
+		}
+}
 }
 
-// get money
-// subract 25 and add 25 to bank
-// check for change
