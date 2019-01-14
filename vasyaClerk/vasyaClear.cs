7e6eaeab0b8abb1e292853f3307e3ce6b1f53d81
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-14 09:42:56 -0500

    works with straight values, not with bills

--- vasyaClerk/vasyaClear.cs
+++ vasyaClerk/vasyaClear.cs
@@ -4,6 +4,20 @@ public class Line
 {
 	public static string Tickets(int[] peopleInLine)
 	{
-		//Your code is here...
+		int bank = 0;
+		foreach(int payment in peopleInLine) {
+			
+			int change = payment;
+			change -= 25;
+			bank += 25;
+			if (bank - payment < 0) {
+				return "NO";
+			}
+		}
+		return "YES";
 	}
 }
+
+// get money
+// subract 25 and add 25 to bank
+// check for change
