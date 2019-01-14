d049dcf8ca299dcfc49e8ef20e365c98f76c9c34
Mikaelabehsera <mikael.abehsera@gmail.com>
2019-01-14 08:37:17 -0500

    passes inital tests

--- highestAndLowest/highestAndLowest.cs
+++ highestAndLowest/highestAndLowest.cs
@@ -4,6 +4,23 @@ public static class Kata
 {
 	public static string HighAndLow(string numbers)
 	{
-		return "throw towel";
+		// split string, store all high and low numbers, return as string
+		int high = 0;
+		int low = 0;
+		string[] list = numbers.Split(" ");
+
+		foreach (string l in list) {
+			int n;
+			Int32.TryParse(l.ToString(), out n);
+			if (n > high) {
+				high = n;
+			} 
+			if (n < low) {
+				low = n;
+			}
+		}
+
+		return high.ToString() + " " + low.ToString();
+
 	}
 }
