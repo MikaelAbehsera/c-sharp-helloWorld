49a6862def5517f1f592796c88991de92046893d
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-20 03:07:09 -0500

    dna changes

--- dnaCompiler/dnaCompiler.cs
+++ dnaCompiler/dnaCompiler.cs
@@ -1,22 +1,34 @@
 using System;
-public class DnaStrand 
+public class DnaStrand
+{
+  public static string MakeComplement(string dna)
+  {
+
+
+    List<string> dnaString = new List<string>();
+
+    for (int i = 0; i < )
     {
-        public static string MakeComplement(string dna)
-        {
-          List<string> dnaString = new List<string>();
 
-          foreach (char d in dna) {
-            if( d.ToString() == "A" ) {
-              dnaString.Add("T");
-            } else if ( d.ToString() == "T" ) {
-              dnaString.Add("A");
-            } else if ( d.ToString() == "G" ) {
-              dnaString.Add("C"); 
-            } else if ( d.ToString() == "C" ) {
-              dnaString.Add("G");
-            }
-          }
-
-          return dna;
-        }
+      if (d.ToString() == "A")
+      {
+        dnaString.Add("T");
+      }
+      else if (d.ToString() == "T")
+      {
+        dnaString.Add("A");
+      }
+      else if (d.ToString() == "G")
+      {
+        dnaString.Add("C");
+      }
+      else if (d.ToString() == "C")
+      {
+        dnaString.Add("G");
+      }
     }
+
+
+    return dna;
+  }
+}
